b3576aa2a7561dd9d98868657ab88fbe6ee7217d
Dave Strus <dave@getfretless.com>
2016-02-01 02:13:57 -0500

    Add Mutant class.

--- lib/mutantcorp/mutant.rb
+++ lib/mutantcorp/mutant.rb
@@ -1,7 +1,25 @@
 module Mutantcorp
   module Mutant
-    def self.hello
-      puts "Hello, mutant!"
+    class Mutant
+      ATTRIBUTE_NAMES = [:real_name, :mutant_name, :power]
+
+      def self.attribute_names
+        ATTRIBUTE_NAMES
+      end
+
+      attr_accessor *ATTRIBUTE_NAMES
+
+      def description
+        "#{mutant_name} (also known as #{real_name}) has an incredible power: #{power}."
+      end
+
+      def attributes
+        attribute_collection = {}
+        ATTRIBUTE_NAMES.each do |attribute_name|
+          attribute_collection[attribute_name] = send(attribute_name)
+        end
+        attribute_collection
+      end
     end
   end
 end

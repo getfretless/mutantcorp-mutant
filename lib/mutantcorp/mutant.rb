70e6262f52d4ad76d0b5e689eb8b25df09ebe0b7
Dave Strus <dave@getfretless.com>
2016-02-01 00:43:05 -0500

    Initial commit.

---
+++ lib/mutantcorp/mutant.rb
@@ -0,0 +1,7 @@
+module Mutantcorp
+  module Mutant
+    def self.hello
+      puts "Hello, mutant!"
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'mutantcorp/mutant'
+
+require 'minitest/autorun'
